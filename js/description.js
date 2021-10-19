a87719e9de26aa3fec693d4e5df2330fa4c046ac
Hajar SERRAD <hajarserrad@gmail.com>
2021-10-20 01:05:51 +0200

    adding js part

---
+++ js/description.js
@@ -0,0 +1,104 @@
+
+
+/*
+
+// qst 1 et 2
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.details').forEach(details=>{
+        details.style.display="none";
+    });   
+    
+});
+
+
+document.addEventListener('click', event => {
+    const element = event.target;
+    if(element.className==='click') {
+       
+        element.style.display="none";
+        element.nextElementSibling.style.display="block";
+    };
+    document.querySelectorAll('.details').forEach(details=>{
+        if(details!=element.nextElementSibling) {
+            details.style.display="none";
+             
+details.previousElementSibling.style.display = "block";
+            
+        }
+    });  
+
+});
+
+*/
+
+
+// qst 3 (code final pour la partie Apparition des descriptions détaillées – 1)
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.details').forEach(details=>{
+        details.style.display="none";
+    });    
+});
+
+
+document.addEventListener('click', event => {
+    const element = event.target;
+    if(element.className==='click') {
+       
+        element.style.display="none";
+        
+
+        element.nextElementSibling.style.display="block";
+
+        let offsetHeight =parseFloat(element.nextElementSibling.offsetHeight);
+        element.nextElementSibling.style.height=0;
+        element.nextElementSibling.style.opacity=0;
+        element.nextElementSibling.style.lineHeight=0;
+        let height=opacity=lineHeight=0;
+        
+       let v = setInterval(function() { 
+            
+           if(height<offsetHeight)
+               height++;
+            
+            if(opacity<1)
+                opacity=opacity+0.05;
+
+            if(lineHeight<1)
+                lineHeight=lineHeight+0.05;
+
+        element.nextElementSibling.style.height=""+height+"px";
+        element.nextElementSibling.style.opacity=parseFloat(opacity);
+        element.nextElementSibling.style.lineHeight=parseFloat(lineHeight);
+
+        if(element.nextElementSibling.style.height==offsetHeight && element.nextElementSibling.style.opacity==1 && element.nextElementSibling.style.lineHeight==1)
+            clearInterval(v);
+     }, 30);
+     
+    };
+         
+    document.querySelectorAll('.details').forEach(details=>{
+        if(details!=element.nextElementSibling) {
+            details.style.display="none";
+            details.previousElementSibling.style.display = "block";
+        }
+    });  
+ 
+     
+
+});
+
+// Select a specified element
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+  });
+
+  $( document ).on( "mousemove", function( event ) {
+    $( "#log" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+    $( ".tooltip" ).css({
+      "left" : event.pageX,
+      "top" : event.pageY
+    });
+  });
